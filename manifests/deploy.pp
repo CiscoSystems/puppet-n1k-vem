99923c090a6a58d2cf69d678583566e43e597d71
mdevaraj <mdevaraj@cisco.com>
2013-08-30 18:24:17 -0700

    Adding ovspackage related changes.

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -1,45 +1,56 @@
 class n1k-vem::deploy {
 
+  $vempackages = ["libnl1" ,"openvswitch-switch"]
+  package { "vempackages":
+    name => $vempackages,
+    ensure => "installed"
+  }
+
+  service { 'n1kv':
+    restart => '/usr/sbin/service n1kv restart'
+  }
+
   package { "libnl1":
-         ensure => installed
+    ensure => installed
   }
 
   file { '/etc/n1kv':
-         owner => 'root',
-         group => 'root',
-         mode  => '664',
-         ensure => directory,
-         require => Package['libnl1']
+    owner => 'root',
+    group => 'root',
+    mode  => '664',
+    ensure => directory,
+    require => Package[$vempackages],
   }
 
   file { $imgfile:
-         owner => 'root',
-         group => 'root',
-         mode => '666',
-         source => "puppet:///files/$imagename",
-         require => File['/etc/n1kv']
+    owner => 'root',
+    group => 'root',
+    mode => '666',
+    source => "puppet:///files/$imagename",
+    require => File['/etc/n1kv'],
   }
 
   package {"nexus1000v":
-        provider => dpkg,
-        ensure => installed,
-        source => $imgfile,
-        require => File[$imgfile]
+    provider => dpkg,
+    ensure => installed,
+    source => $imgfile,
+    require => File[$imgfile]
   }
 
   file {"/etc/n1kv/n1kv.conf":
-        owner => 'root',
-        group => 'root',
-        mode => '666',
-        source => "puppet:///files/${n1kconfname}_n1k.conf",
-        require => Package['nexus1000v']
+    owner => 'root',
+    group => 'root',
+    mode => '666',
+    source => "puppet:///files/${n1kconfname}_n1k.conf",
+    require => Package['nexus1000v'],
+    notify => Service['n1kv']
   }
 
   exec {"launch_vem":
-       command => "/usr/sbin/service n1kv start",
-       unless => "/sbin/vemcmd show card"
+    command => "/usr/sbin/service n1kv start",
+    unless => "/sbin/vemcmd show card"
   }
-  
-  File['/etc/n1kv/n1kv.conf'] -> Exec['launch_vem']
+
+  Package[libnl1] -> File['/etc/n1kv'] -> File[$imgfile] -> Package["nexus1000v"] -> File['/etc/n1kv/n1kv.conf'] -> Exec['launch_vem']
 
 }
