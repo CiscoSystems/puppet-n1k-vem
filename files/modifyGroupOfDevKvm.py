8b10962044332c9925e13aff2757ed996648cc9f
mdevaraj <mdevaraj@cisco.com>
2013-11-06 14:34:52 -0800

    Adding chect to verify if /dev/kvm is present

--- files/modifyGroupOfDevKvm.py
+++ files/modifyGroupOfDevKvm.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
 import grp, pwd, os, tempfile
 
-log_file = tempfile.NamedTemporaryFile(delete=False)
-stat_info = os.stat('/dev/kvm')
-uid = stat_info.st_uid
-gid = stat_info.st_gid
+fname = '/dev/kvm'
+if os.path.isfile(fname) == True:
+  log_file = tempfile.NamedTemporaryFile(delete=False)
+  stat_info = os.stat(fname)
+  uid = stat_info.st_uid
+  gid = stat_info.st_gid
 
-user = pwd.getpwuid(uid)[0]
-group = grp.getgrgid(gid)[0]
-str = "user is " + user + ", group is " + group + "\n"
-log_file.write(str)
+  user = pwd.getpwuid(uid)[0]
+  group = grp.getgrgid(gid)[0]
+  str = "user is " + user + ", group is " + group + "\n"
+  log_file.write(str)
 
-if group == "root":
-  log_file.write("group is root\n")
-  log_file.write("Doing rmmod kvm_intel kvm\n")
-  os.system("rmmod kvm_intel kvm")
-  log_file.write("Doing modprobe kvm_intel\n")
-  os.system("modprobe kvm_intel")
-else:
-  log_file.write("group is not root\n")
+  if group == "root":
+    log_file.write("group is root\n")
+    log_file.write("Doing rmmod kvm_intel kvm\n")
+    os.system("rmmod kvm_intel kvm")
+    log_file.write("Doing modprobe kvm_intel\n")
+    os.system("modprobe kvm_intel")
+  else:
+    log_file.write("group is not root\n")
 
-log_file.close()
+  log_file.close()
