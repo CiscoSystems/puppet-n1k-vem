dd7d3d2f8be65fceb4357cb5537cb13f6ae0af09
Kiran Chunduri <nagchund@cisco.com>
2014-06-25 08:45:51 -0700

    single manifest file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# == Class: n1k-vem 
+# == Class: n1kv-vem 
 #
 # Deploy N1KV VEM on compute and network nodes.
 #
@@ -41,7 +41,7 @@
 #     n1kv_version ==>'latest'. Instead specific version can be specified. 
 #       Not applicable if 'n1kv_source' is a file. (Option-B above)
 #
-class n1k-vem (
+class n1kv-vem (
     $n1kv_vsm_ip = "10.10.10.250",
     $n1kv_vsm_domain_id = 1000,
     $host_mgmt_intf = "eth1",
@@ -61,11 +61,7 @@ class n1k-vem (
   $node_type = $additional_params[node_type]
   $vem_pkgver = $additional_params[n1kv_version]
 
-  if $::osfamily == 'Ubuntu' {
-    $puppet_file_location = "/etc/puppet/files/"
-  } elsif $::osfamily == 'Redhat' {
-    $puppet_file_location = "$pwd/../files"
-  }
+  $puppet_file_location = "/etc/puppet/files"
 
   if $vemimage != "" {
     if inline_template("<%= vemimage.include?('ftp') %>") == "true" {
@@ -81,5 +77,102 @@ class n1k-vem (
     }
   }
 
-  include n1k-vem::deploy
+
+  $n1kv_pkg = "nexus1000v"
+  $puppet_file_uri = "puppet:///extra_files/$imagename"
+  case $::osfamily {
+    'Redhat': {
+      $pkg_provider = "rpm"
+      $libnl_pkg = "libnl"
+      $ovs_pkg = "openvswitch"
+      $cmd_service = "/sbin/service"
+    }
+    'Ubuntu': {
+      $pkg_provider = "dpkg"
+      $libnl_pkg = "libnl1"
+      $ovs_pkg = "openvswitch-switch"
+      $cmd_service = "/usr/sbin/service"
+    }
+    default: {
+      fail( "${::osfamily} not yet supported by n1k-vem")
+    }
+  }
+
+  package { 'libnl':
+    name => $libnl_pkg,
+    ensure => 'installed'
+  }
+
+  package { 'openvswitch':
+    name => $ovs_pkg,
+    ensure => 'installed'
+  }
+
+  file { '/etc/n1kv':
+    owner => 'root',
+    group => 'root',
+    mode  => '664',
+    ensure => directory
+  }
+
+  #specify template corresponding to 'n1kv.conf'
+
+  file {'/etc/n1kv/n1kv.conf':
+    owner => 'root',
+    group => 'root',
+    mode => '666',
+    content => template('n1kv-vem/n1kv.conf.erb'),
+    require => File['/etc/n1kv'],
+    ensure => present
+  }
+
+  if $vemimage_uri == 'local' {
+    file { $imgfile:
+      owner => 'root',
+      group => 'root',
+      mode => '666',
+      source => $puppet_file_uri,
+      require => File['/etc/n1kv'],
+    }
+    package {'nexus1000v':
+      provider => $pkg_provider,
+      name => $n1kv_pkg,
+      ensure => latest,
+      source => $imgfile,
+      require => File[$imgfile]
+    }
+  } else {
+    yumrepo { 'cisco-foreman':
+      baseurl => $vemimage,
+      descr => 'Cisco Internal repo for Foreman',
+      enabled => 1,
+      gpgcheck => 1,
+      gpgkey => "$vemimage/RPM-GPG-KEY"
+      #proxy => '_none_',
+    }
+    package {'nexus1000v':
+      name => $n1kv_pkg,
+      ensure => $n1kv_pkgver,
+    }
+  }
+
+  service { nexus1000v:
+    ensure => running,
+    subscribe => File['/etc/n1kv/n1kv.conf'],
+    restart => "$cmd_service nexus1000v restart"
+  }
+
+  if $isMultipleVtepInSameSubnet == 'true' {
+    $my_sysctl_settings = {
+      "net.ipv4.conf.default.rp_filter" => { value => 2 },
+      "net.ipv4.conf.all.rp_filter" => { value => 2 },
+      "net.ipv4.conf.default.arp_ignore" => { value => 1 },
+      "net.ipv4.conf.all.arp_ignore" => { value => 1 },
+      "net.ipv4.conf.all.arp_announce" => { value => 2 },
+      "net.ipv4.conf.default.arp_announce" => { value => 2 },
+    }
+    create_resources(sysctl::value,$my_sysctl_settings)
+  }
+
+  Package['libnl'] -> Package['openvswitch'] -> File['/etc/n1kv/n1kv.conf'] -> Package['nexus1000v'] -> Service[nexus1000v]
 }
